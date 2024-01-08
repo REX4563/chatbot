31244060d2f71ff9ac5b552d3fa7bfd7c6a06f60
REX4563 <98383232+REX4563@users.noreply.github.com>
2024-01-09 01:14:11 +0530

    script.js

--- script.js
+++ script.js
@@ -38,6 +38,9 @@ const getChatResponse = async (incomingChatDiv) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': "*",
+            'Access-Control-Allow-Methods': "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            'Access-Control-Allow-Headers': "Origin, Content-Type, X-Auth-Token"
         },
         body: JSON.stringify({
             userText: userText,
